161bcab66069b135a0ed50ecc70fae470f146ae0
XueJie1 <3124792285@qq.com>
2025-10-27 17:18:19 +0800

    project file init

---
+++ src/main/java/com/novaforum/nova_forum/NovaForumApplication.java
@@ -0,0 +1,13 @@
+package com.novaforum.nova_forum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovaForumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NovaForumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/novaforum/nova_forum/NovaForumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.novaforum.nova_forum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovaForumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
